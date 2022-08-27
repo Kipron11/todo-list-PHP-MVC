899fc6503a549947d7f3e3dc2b48c0766d45def3
Nikita <kipron11@gmail.com>
2022-08-27 20:02:04 +0300

    Updated features in Router.php, implemented Response class

--- core/Application.php
+++ core/Application.php
@@ -4,18 +4,24 @@ namespace app\core;
 
 class Application
 {
+	public static string $ROOT_DIR;
+	public Router $router;
+	public Request $request;
+	public Response $response;
+	public static Application $app;
 
-    public Router $router;
-    public Request $request;
 
+	public function __construct($rootPath)
+	{
+		self::$ROOT_DIR = $rootPath;
+		self::$app = $this;
+		$this->request = new Request();
+		$this->response = new Response();
+		$this->router = new Router($this->request, $this->response);
+	}
 
-    public function __construct()
-    {
-        $this->request = new Request();
-        $this->router = new Router($this->request);
-    }
-
-    public function run(){
-    $this->router->resolve();
-    }
+	public function run()
+	{
+		echo $this->router->resolve();
+	}
 }
--- public/index.php
+++ public/index.php
@@ -19,15 +19,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use app\core\Application;
 
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
 
-$app->router->get('/', function () {
-	return 'HomePage';
-});
-$app->router->get('/todos', function () {
-	return 'Todos';
-});
+$app->router->get('/', 'home');
+$app->router->get('/todos', 'todos');
 
 
 $app->run();
