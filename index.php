497e440e1b768b0f74437ba379b1a3dd3bf82a5b
Nikita <kipron11@gmail.com>
2022-08-27 18:17:01 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//TODO Custom Routing,
+//TODO  Composer,
+//TODO Controllers,
+//TODO View/Layouts,
+//TODO Models,
+//TODO Migrations
+//TODO Form Widget classes,
+//TODO Processing of request data,
+//TODO  validations,
+//TODO  Reg/Login,
+//TODO  Simple Active Record,
+//TODO  Session Flash messages,
+//TODO  MiddleWares,
+//TODO App events,
+//TODO  Framework reusable/installable core
+
+
